00705b69b9102cfa17791ee7436ba9fd4c748fca
John Kalberer <jlkalberer@gmail.com>
2017-01-08 11:21:25 -0800

    Removing extra functions from utilities.

--- src/lib/utilities.js
+++ src/lib/utilities.js
@@ -31,166 +31,76 @@ import path from 'path';
 import settings from '../settings.js';
 import ursa from 'ursa';
 
-var utilities = {
-    /**
-     * Surely there is a better way to do this.
-     * NOTE! This function does NOT short-circuit when an in-equality is detected.  This is
-     * to avoid timing attacks.
-     * @param left
-     * @param right
-     */
-    bufferCompare: function (left: Buffer, right: Buffer): boolean {
-      if (left === null && right === null) {
-          return true;
-      } else if (left === null || right === null) {
-          return false;
-      }
+class utilities {
+  /**
+   * Surely there is a better way to do this.
+   * NOTE! This function does NOT short-circuit when an in-equality is
+     detected.  This is
+   * to avoid timing attacks.
+   * @param left
+   * @param right
+   */
+  static bufferCompare = (left: Buffer, right: Buffer): boolean => {
+    if (left === null && right === null) {
+      return true;
+    } else if (left === null || right === null) {
+      return false;
+    }
 
-      if (!Buffer.isBuffer(left)) {
-          left = new Buffer(left);
-      }
-      if (!Buffer.isBuffer(right)) {
-          right = new Buffer(right);
-      }
+    if (!Buffer.isBuffer(left)) {
+        left = new Buffer(left);
+    }
+    if (!Buffer.isBuffer(right)) {
+        right = new Buffer(right);
+    }
 
-      //logger.log('left: ', left.toString('hex'), ' right: ', right.toString('hex'));
+    return Buffer.compare(left, right) == 0;
+  }
 
-      return Buffer.compare(left, right) == 0;
-    },
+  static toHexString = (value: number): string => {
+    return (value < 10 ? '0' : '') + value.toString(16);
+  };
 
-    /**
-     * Iterates over the properties of the right object, checking to make
-     * sure the properties on the left object match.
-     * @param left
-     * @param right
-     */
-    leftHasRightFilter: function (
-      left: Object,
-      right: Object,
-    ): boolean {
-        if (!left && !right) {
-            return true;
-        }
-
-        for (var prop in right) {
-          if (!right.hasOwnProperty(prop)) {
-              continue;
-          }
-          if (left[prop] !== right[prop]) {
-            return false;
-          }
-        }
-
-        return true;
-    },
-
-    readBuffer: function (fileName: string): Buffer {
-      if (!fs.existsSync(fileName)) {
-        throw Error('File does not exist', fileName);
-      }
-
-      return fs.readFileSync(fileName);
-    },
-
-    toHexString: function (value: number) {
-      return (value < 10 ? '0' : '') + value.toString(16);
-    },
-
-    getFilenameExt: function (fileName: string): string {
-      if (!fileName || !fileName.length) {
-        return fileName;
-      }
-
-      const index = fileName.lastIndexOf('.');
-      if (index >= 0) {
-        return fileName.substr(index);
-      } else {
-        return fileName;
-      }
-    },
-
-    filenameNoExt: function (fileName: string): string {
-      if (!fileName || (fileName.length === 0)) {
-        return fileName;
-      }
-
-      const index = fileName.lastIndexOf('.');
-      if (index >= 0) {
-        return fileName.substr(0, index);
-      } else {
-        return fileName;
-      }
-    },
-
-    get_core_key: function(coreId: string): Object {
-      const keyFile = path.join(
-        global.settings && global.settings.DEVICE_DIRECTORY || settings.DEVICE_DIRECTORY,
-        coreId + '.pub.pem',
-      );
-      if (!fs.existsSync(keyFile)) {
-        throw `Expected to find public key for core ${coreId} at ${keyFile}`;
-      }
-      const keyStr = fs.readFileSync(keyFile).toString();
-      return ursa.createPublicKey(keyStr, 'binary');
-    },
-
-  	save_handshake_key: function(coreId: string, pem: string) {
-  		const keyFile = path.join(
-        global.settings && global.settings.DEVICE_DIRECTORY || settings.DEVICE_DIRECTORY,
-        coreId + '_handshake.pub.pem',
-      );
-  		if (fs.existsSync(keyFile)) {
-        return;
-      }
-
-  		logger.log(
-        'I saved a key given during the handshake, (remove the _handshake ' +
-          'from the filename to accept this device)',
-        keyFile,
-      );
-  		fs.writeFileSync(keyFile, pem);
-  	},
-
-    /**
-	 * base64 encodes raw binary into
-	 * "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHzg9dPG03Kv4NkS3N0xJfU8lT1M+s9HTs75DE1tpwXfU4GkfaLLr04j6jFpMeeggKCgWJsKyIAR9CNlVHC1IUYeejEJQCe6JReTQlq9F6bioK84nc9QsFTpiCIqeTAZE4t6Di5pF8qrUgQvREHrl4Nw0DR7ECODgxc/r5+XFh9wIDAQAB"
-	 * then formats into PEM format:
-	 *
-	 * //-----BEGIN PUBLIC KEY-----
-	 * //MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHzg9dPG03Kv4NkS3N0xJfU8lT
-	 * //1M+s9HTs75DE1tpwXfU4GkfaLLr04j6jFpMeeggKCgWJsKyIAR9CNlVHC1IUYeej
-	 * //EJQCe6JReTQlq9F6bioK84nc9QsFTpiCIqeTAZE4t6Di5pF8qrUgQvREHrl4Nw0D
-	 * //R7ECODgxc/r5+XFh9wIDAQAB
-	 * //-----END PUBLIC KEY-----
-	 *
-	 * @param buf
-	 * @returns {*}
-	 */
-	convertDERtoPEM: function(buffer: ?Buffer): ?string {
-		if (!buffer || !buffer.length) {
-			return null;
-		}
+  /**
+   * base64 encodes raw binary into
+   * "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHzg9dPG03Kv4NkS3N0xJfU8lT1M+s9HTs75
+      DE1tpwXfU4GkfaLLr04j6jFpMeeggKCgWJsKyIAR9CNlVHC1IUYeejEJQCe6JReTQlq9F6bioK
+      84nc9QsFTpiCIqeTAZE4t6Di5pF8qrUgQvREHrl4Nw0DR7ECODgxc/r5+XFh9wIDAQAB"
+   * then formats into PEM format:
+   *
+   * //-----BEGIN PUBLIC KEY-----
+   * //MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHzg9dPG03Kv4NkS3N0xJfU8lT
+   * //1M+s9HTs75DE1tpwXfU4GkfaLLr04j6jFpMeeggKCgWJsKyIAR9CNlVHC1IUYeej
+   * //EJQCe6JReTQlq9F6bioK84nc9QsFTpiCIqeTAZE4t6Di5pF8qrUgQvREHrl4Nw0D
+   * //R7ECODgxc/r5+XFh9wIDAQAB
+   * //-----END PUBLIC KEY-----
+   *
+   * @param buf
+   * @returns {*}
+   */
+  static convertDERtoPEM = (buffer: ?Buffer): ?string => {
+  	if (!buffer || !buffer.length) {
+  		return null;
+  	}
 
     const bufferString = buffer.toString('base64');
-		try {
-			const lines = [
+  	try {
+  		const lines = [
         '-----BEGIN PUBLIC KEY-----',
         ...(bufferString.match(/.{1,64}/g) || []),
         '-----END PUBLIC KEY-----',
       ];
-			return lines.join('\n');
-		} catch(exception) {
-			logger.error(
+  		return lines.join('\n');
+  	} catch(exception) {
+  		logger.error(
         'error converting DER to PEM, was: ' +
           bufferString +
           ' ' +
           exception,
       );
-		}
-		return null;
-	},
-
-    foo: null
+  	}
+  	return null;
+  };
 };
 
 export default utilities;
