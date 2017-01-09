f18564d07a6ddea4f90ed1cdfe5d47968ff7dd4f
AntonPuko <antonpukowebdev@gmail.com>
2017-01-10 01:59:53 +0200

    fix formatting nit, rebuild deviceServer.js

--- src/server/DeviceServer.js
+++ src/server/DeviceServer.js
@@ -386,7 +386,7 @@ class DeviceServer {
         device.sendReply('SubscribeAck', message.getId());
         logger.log(
           `device with ID ${deviceID} wasn't subscribed to` +
-        `${messageName} MY_DEVICES event: the device is unclaimed.`
+        `${messageName} MY_DEVICES event: the device is unclaimed.`,
         );
         return;
       }
